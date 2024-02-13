5edc8ddf0fa9d170bc1e4e306ea614880e8e4f3a
TheBeeBoi <architgis@gmail.com>
2024-02-13 22:41:51 +0530

    uhh idk

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,11 @@
+function fish_title
+    if count $argv > /dev/null
+        echo -n "running " (basename $argv) " off fish on kitty in $PWD - waybarprefix"
+    else
+        echo -n "fish on kitty in $PWD - waybarprefix"
+    end
+end
+
 if status is-interactive
     alias nerdfetch='/usr/bin/nerdfetch && echo'
     alias pru='paru'
@@ -5,4 +13,5 @@ if status is-interactive
     alias clear='/usr/bin/clear && fish_greeting'
     starship init fish | source
 end
+
 alias nvim=lvim
